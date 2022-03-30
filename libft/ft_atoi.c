f879e9cf9c976ae8687b5c45d1874cecdafe0fc2
Jonas Subel <jsubel@students.42wolfsburg.de>
2022-03-30 11:57:19 +0200

    added libft back

---
+++ libft/ft_atoi.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 10:05:12 by jsubel            #+#    #+#             */
+/*   Updated: 2021/12/08 15:14:37 by jsubel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_whitespace(char c);
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nbr;
+	int	sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	while (is_whitespace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = 10 * nbr + (str[i] - '0');
+		i++;
+	}
+	return (nbr * sign);
+}
+
+static int	is_whitespace(char c)
+{
+	if (c == ' ')
+		return (1);
+	if (c == '\t')
+		return (1);
+	if (c == '\r')
+		return (1);
+	if (c == '\n')
+		return (1);
+	if (c == '\v')
+		return (1);
+	if (c == '\f')
+		return (1);
+	return (0);
+}
---
+++ libft/ft_lstnew.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsubel <jsubel@student.42wolfsburg.de >    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 12:56:12 by jsubel            #+#    #+#             */
+/*   Updated: 2022/03/28 08:58:03 by jsubel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(int content)
+{
+	t_list	*new_element;
+
+	new_element = (t_list *)ft_calloc(1, sizeof(t_list *));
+	if (!new_element)
+	{
+		free(new_element);
+		return (NULL);
+	}
+	new_element->content = content;
+	new_element->index = 0;
+	new_element->next = NULL;
+	return (new_element);
+}
